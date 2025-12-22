6226d5b46e293d9b776d4344b9c5c8c8f25af260
StanyslasFruchard <stanyslas.fruchard@alltricks.com>
2025-12-22 19:20:28 +0100

    handle , prices

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Galactic Tycoons profit tabs
 // @namespace    https://g2.galactictycoons.com/
-// @version      1.3.3
+// @version      1.3.4
 // @description  try to take over the galactic world!
 // @author       Leyla the pro
 // @match        https://g2.galactictycoons.com/*
@@ -61,7 +61,7 @@
     }
 
     function createSpan(price, classToDisplay) {
-        var priceToDsplay = price.toFixed(2).toString().split(".")
+        var priceToDsplay = price.toFixed(2).toString().split("[,.]")
         if (price > 0) {
             priceToDsplay[0] = '+' + priceToDsplay[0]
         }
@@ -70,7 +70,7 @@
     `
     }
     function createSpanPerHour(price, classToDisplay) {
-        var priceToDsplay = price.toFixed(2).toString().split(".")
+        var priceToDsplay = price.toFixed(2).toString().split("[,.]")
         if (price > 0) {
             priceToDsplay[0] = '+' + priceToDsplay[0]
         }
